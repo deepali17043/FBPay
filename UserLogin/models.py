71a4ce29b704fa4e31704c3524bde22cd357495b
Deepali <deepali17043@iiitd.ac.in>
2019-09-25 23:11:54 +0530

    change in attributes for user model

--- UserLogin/models.py
+++ UserLogin/models.py
@@ -4,9 +4,8 @@ from django.db import models
 
 # When including different types of users, convert this class into an abstract class
 class User (models.Model):
-    username = models.TextField()
-    password = models.TextField()
-    name = models.TextField()
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     Birthday = models.CharField(max_length=10)
     WalletMoney = models.IntegerField()
-
